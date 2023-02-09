569e7a4f2e6bd8fe1cd558ba867defaf0c46d1f3
weekend <weekend45@163.com>
2023-02-09 15:17:20 +0800

    相册选择器改用PictureSelector，去掉原来相册代码，解决安卓10，11以及以上版本的权限相关问题

--- CroppDemo/app/src/main/java/com/example/croppdemo/ImageEditSampleActivity.kt
+++ CroppDemo/app/src/main/java/com/example/croppdemo/ImageEditSampleActivity.kt
@@ -2,16 +2,24 @@ package com.example.croppdemo
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.text.TextUtils
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.croppdemo.util.ContextHelper
+import com.example.croppdemo.util.GlideEngine
+import com.luck.picture.lib.basic.PictureSelector
+import com.luck.picture.lib.config.PictureSelectionConfig.selectorStyle
+import com.luck.picture.lib.config.SelectMimeType
+import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.interfaces.OnResultCallbackListener
 import kotlinx.android.synthetic.main.activity_image_edit_sample.*
-import me.minetsh.imaging.IMGEditActivity
-import me.minetsh.imaging.IMGGalleryActivity
-import me.minetsh.imaging.gallery.model.IMGChooseMode
-import me.minetsh.imaging.gallery.model.IMGImageInfo
+import me.minetsh.imaging.IMGCropUtil
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -19,7 +27,7 @@ import java.util.*
 
 class ImageEditSampleActivity : AppCompatActivity() {
     private val mFunction by lazy { intent.getSerializableExtra("function") as? String }
-    private var mImageFile: File? = null
+    private var pickUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +38,6 @@ class ImageEditSampleActivity : AppCompatActivity() {
         }
     }
 
-    // 使用系统当前时间加以调整作为照片的名称
     private fun getPhotoFile(): File {
         val dir = Environment.getExternalStorageDirectory()
         val myDir = File(dir, "/Pictures/cropDemo")
@@ -39,6 +46,7 @@ class ImageEditSampleActivity : AppCompatActivity() {
         return File(dirName, getPhotoFileName())
     }
 
+    // 使用系统当前时间加以调整作为照片的名称
     private fun getPhotoFileName(): String {
         val date = Date(System.currentTimeMillis())
         val dateFormat = SimpleDateFormat(
@@ -47,36 +55,57 @@ class ImageEditSampleActivity : AppCompatActivity() {
         return dateFormat.format(date) + ".jpg"
     }
 
-    private fun chooseImages() {
-        startActivityForResult(
-            IMGGalleryActivity.newIntent(
-                this, IMGChooseMode.Builder()
-                    .setSingleChoose(true)
-                    .build()
-            ),
-            REQ_IMAGE_CHOOSE
-        )
+    private fun getPathFile(dir: String): File? {
+        return ContextHelper.getAppContext().getExternalFilesDir(File.separatorChar + dir)
     }
 
-    private fun onChooseImages(images: List<IMGImageInfo>?) {
-        val image = images?.get(0)
-        if (image != null) {
-
-            sdv_image.setImageURI(image.uri, null)
-
-            mImageFile = getPhotoFile();
-
-            startActivityForResult(
-                Intent(this, IMGEditActivity::class.java)
-                    .putExtra(IMGEditActivity.EXTRA_IMAGE_URI, image.uri)
-                    .putExtra(IMGEditActivity.EXTRA_IMAGE_FUNCTION, mFunction)
-                    .putExtra(
-                        IMGEditActivity.EXTRA_IMAGE_SAVE_PATH,
-                        mImageFile?.absolutePath
-                    ),
-                REQ_IMAGE_EDIT
-            )
+    fun getCropPhotoFileUri(context: Context): Uri {
+        return if (Build.VERSION.SDK_INT < 29) {
+            Uri.fromFile(getPhotoFile())
+        } else {        //安卓10以上采用了分区存储，使用私有目录来保存裁剪后的图片即可
+            val dir = getPathFile("crop") ?: context.filesDir
+            dir?.mkdirs()
+            Uri.fromFile(File(dir, getPhotoFileName()))
         }
+    }
+
+    private fun chooseImages() {
+        PictureSelector.create(this)
+            .openGallery(SelectMimeType.ofImage())
+            .setMaxSelectNum(1)
+            .setSelectorUIStyle(selectorStyle)//设置动画效果
+            .setImageEngine(GlideEngine.createGlideEngine())
+            .forResult(resultCallback)
+    }
+
+    private var resultCallback: OnResultCallbackListener<LocalMedia> = object :
+        OnResultCallbackListener<LocalMedia> {
+        override fun onResult(result: java.util.ArrayList<LocalMedia>?) {
+            if (result != null && result.size != 0) {
+                val localMedia = result[0]
+                val thePath = if (localMedia.isCompressed)localMedia.compressPath else localMedia.realPath
+
+                if (TextUtils.isEmpty(thePath)) {
+                    return  //路径为空
+                }
+                val file = File(thePath)
+                if (!file.exists()) {//文件不存在
+                    return
+                }
+                Log.d("weekend", "crop src file path=$thePath")
+                pickUri = getCropPhotoFileUri(this@ImageEditSampleActivity)
+
+                IMGCropUtil.StartIMGCrop(
+                    this@ImageEditSampleActivity,
+                    IMGCropUtil.getImageUri(this@ImageEditSampleActivity, file),
+                    IMGCropUtil.getInstance().getPath(this@ImageEditSampleActivity, pickUri),
+                    mFunction,
+                    IMGCropUtil.INTENT_CROP
+                )
+            }
+        }
+
+        override fun onCancel() {}
     }
 
     private fun showTips(path: String?) {
@@ -90,18 +119,13 @@ class ImageEditSampleActivity : AppCompatActivity() {
     }
 
     private fun onImageEditDone() {
-        sdv_image_edit.setImageURI(Uri.fromFile(mImageFile), null)
-        showTips(mImageFile?.absolutePath);
+        sdv_image_edit.setImageURI(pickUri, null)
+        showTips(IMGCropUtil.getInstance().getPath(this@ImageEditSampleActivity, pickUri));
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         when (requestCode) {
-            REQ_IMAGE_CHOOSE -> {
-                if (resultCode == Activity.RESULT_OK) {
-                    onChooseImages(IMGGalleryActivity.getImageInfos(data))
-                }
-            }
-            REQ_IMAGE_EDIT -> {
+            IMGCropUtil.INTENT_CROP -> {
                 if (resultCode == Activity.RESULT_OK) {
                     onImageEditDone()
                 }
--- CroppDemo/app/src/main/java/com/example/croppdemo/MainActivity.kt
+++ CroppDemo/app/src/main/java/com/example/croppdemo/MainActivity.kt
@@ -5,10 +5,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-const val REQ_IMAGE_EDIT = 1
-
-const val REQ_IMAGE_CHOOSE = 2
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
